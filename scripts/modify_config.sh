531010d643cdaedacdf94e7cf3b1dd7468d53afa
adoreblvnk <83258252+adoreblvnk@users.noreply.github.com>
2024-06-01 18:01:28 +0800

    feat: helper script for apt packages

---
+++ scripts/modify_config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# /usr/bin
+
+config="config.json"
+deps_file="deps.list"
+install_file="install.list"
+
+deps_to_config() {
+  local deps=$(grep '\S' $deps_file | jq -nR '[inputs]')
+  cat <<<$(
+    jq --argjson deps "$deps" '.check.apt = $deps' $config
+  ) >$config
+}
+
+config_to_deps() {
+  jq -r '.check.apt[]' $config >$deps_file
+}
+
+install_to_config() {
+  local install=$(grep '\S' $install_file | jq -nR '[inputs]')
+  cat <<<$(
+    jq --argjson install "$install" '.install.apt = $install' $config
+  ) >$config
+}
+
+config_to_install() {
+  jq -r '.install.apt[]' $config >$install_file
+}
+
+install_to_config
